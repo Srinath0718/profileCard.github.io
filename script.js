72ca4d3351415b9bd9b814c59347ec8825af876d
Srinath0718 <srinathprasath@gmail.com>
2021-05-25 11:05:34 +0530

    my first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const cancel = document.querySelector(".cancel");
+const arr=document.querySelector(".arr-container");
+const left_container = document.querySelector('.left-container');
+arr.addEventListener("click",()=>{
+  arr.classList.add("active-arr");
+  if(left_container.classList.contains("off")){
+    left_container.classList.remove("off");
+    left_container.classList.add("active");
+    
+  }
+});
+
+
+cancel.addEventListener("click",()=>{
+  arr.classList.add("active-arr");
+  if(left_container.classList.contains("active")){
+    left_container.classList.remove("active");
+    left_container.classList.add("off");
+  }
+});
